f01f926c095371ef8ffba0b734ade4f46d6e9461
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2021-09-27 22:15:32 -0300

    First functional commit

---
+++ Template/main.cpp
@@ -0,0 +1,8 @@
+#include <Lunaris/utility.h>
+#include <Lunaris/graphics.h>
+#include <Lunaris/events.h>
+#include <Lunaris/audio.h>
+
+#include "../Lunaris/Projects/__shared/shared.ipp" // the test
+
+// of course remove the line above and start with an int main() like any other program do.
